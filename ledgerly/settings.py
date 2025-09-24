792d76cd7c7106b8364a026cf085a8639eed2d9b
Jakeymarsh1 <Jake.o.marsh@hotmail.co.uk>
2025-09-24 15:13:21 +0100

    updated allowed hosts

--- ledgerly/settings.py
+++ ledgerly/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-from env import SECRET_KEY
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +21,15 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = SECRET_KEY
+try:
+    from env import SECRET_KEY
+except ImportError:
+    SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['ledgerly-90931141abd8.herokuapp.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost', 'ledgerly-90931141abd8.herokuapp.com']
 
 CSRF_TRUSTED_ORIGINS = ['https://*.herokuapp.com']
 
