34bb43df2e5c921767eb8893891b4bad0e52e35a
Jakeymarsh1 <Jake.o.marsh@hotmail.co.uk>
2025-10-13 14:08:31 +0100

    changed admin panel options for better selection availablilty

--- expenses/models.py
+++ expenses/models.py
@@ -25,6 +25,9 @@ class Category(models.Model):
     def __str__(self):
         return self.name
 
+    class Meta:
+        verbose_name_plural = 'Catergories'
+
 
 class Transaction(models.Model):
     """I store a single income or outgoing entry recorded by a user."""
